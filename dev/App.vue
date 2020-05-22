671498372fdf6ec3b821d59c2c9ed7045b56ebcd
Gareth Fuller <gareth.a.f@gmail.com>
2020-05-22 15:06:28 +0100

    Setup dev app with tailwind

---
+++ dev/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="p-8">
+    <h1 class="text-black text-3xl mb-8">Play with Bitbond components here</h1> 
+
+    <bb-text-input v-model="text" label="A text input" placeholder="test me..." />
+    <bb-btn>Click me</bb-btn>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+
+  data: () => ({
+    text: ''
+  }),
+
+  watch: {
+    text(newVal) {
+      console.log(newVal)
+    }
+  }
+}
+</script>
