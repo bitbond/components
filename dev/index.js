c436fb589327a3e8759f3033b9d7dcf9673f17f3
Gareth Fuller <gareth.a.f@gmail.com>
2020-05-22 15:06:28 +0100

    Setup dev app with tailwind

---
+++ dev/index.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import bbComponents from '../src/entry'
+
+Vue.use(bbComponents)
+
+import "./assets/css/tailwind.css";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          100: '#E6F6FE',
+          200: '#C0EAFC',
+          300: '#99DDFB',
+          400: '#4DC3F7',
+          500: '#01A9F4',
+          600: '#0198DC',
+          700: '#016592',
+          800: '#004C6E',
+          900: '#003349',
+        }
+      }
+    },
+  },
+  variants: {},
+  plugins: [],
+}
