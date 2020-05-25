b01ec5466eecd62879c00077f79943d51463aa44
Melania Moga <galea.melania@gmail.com>
2020-05-25 11:52:28 +0300

    Fix select input change listener

--- dev/App.vue
+++ dev/App.vue
@@ -1,24 +1,31 @@
 <template>
   <div class="p-8">
-    <h1 class="font-black text-3xl mb-8">Play with Bitbond components here</h1> 
+    <h1 class="font-black text-3xl mb-8">Play with Bitbond components here</h1>
 
     <bb-text-input v-model="text" label="A text input" placeholder="test me..." />
     <bb-btn>Click me</bb-btn>
+
+    <bb-select-input v-model="select" :options="['a', 'b', 'c']" />
   </div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
 
   data: () => ({
-    text: ''
+    text: "",
+    select: "a"
   }),
 
   watch: {
     text(newVal) {
-      console.log(newVal)
+      console.log(newVal);
+    },
+
+    select(newVal) {
+      console.log(newVal);
     }
   }
-}
+};
 </script>
--- src/components/bbSelectInput.vue
+++ src/components/bbSelectInput.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="flex flex-col mb-6">
-    <label v-if="label" :for="name" :class="labelClasses">{{ label }}
+    <label v-if="label" :for="name" :class="labelClasses">
+      {{ label }}
       <span v-if="required">*</span>
     </label>
     <label v-else :for="name" :class="labelClasses">
@@ -13,70 +14,63 @@
       v-bind="$attrs"
       v-on="listeners"
     >
-      <option v-for="(option, i) in options" :key="i" :value="valFor(option)">
-        {{ textFor(option) }}
-      </option>
+      <option v-for="(option, i) in options" :key="i" :value="valFor(option)">{{ textFor(option) }}</option>
     </select>
     <div :class="['relative', labelClasses]">
-      <div
-        v-show="hasError"
-        class="text-red-500 text-sm absolute"
-      >
-        {{ errors[0] }}
-      </div>
+      <div v-show="hasError" class="text-red-500 text-sm absolute">{{ errors[0] }}</div>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'bbSelectInput',
+  name: "bbSelectInput",
 
   props: {
     options: { type: Array, required: true },
-    value: { type: [String, Number], default: '' },
+    value: { type: [String, Number], default: "" },
     label: { type: String, default: null },
-    name: { type: String, default: 'select' },
+    name: { type: String, default: "select" },
     required: { type: Boolean, default: false },
     rules: { type: Array },
-    validateOn: { type: String, default: 'change' },
+    validateOn: { type: String, default: "change" },
     size: {
       type: String,
-      default: 'medium',
-      validator: value => ['small', 'medium', 'large'].indexOf(value) !== -1,
+      default: "medium",
+      validator: value => ["small", "medium", "large"].indexOf(value) !== -1
     }
   },
 
   data() {
     return {
       errors: []
-    }
+    };
   },
 
   computed: {
     listeners() {
-      let vm = this
+      let vm = this;
       return {
         ...this.$listeners,
-        change: event => {
-          vm.$emit('input', event.target.value)
-          vm.validate(event.target.value)
+        input: event => {
+          vm.$emit("input", event.target.value);
+          vm.validate(event.target.value);
         }
-      }
+      };
     },
 
     hasError() {
-      return this.errors.length > 0
+      return this.errors.length > 0;
     },
 
     textSize() {
       switch (this.size) {
-        case 'large':
-          return 'text-lg'
-        case 'small':
-          return 'text-sm'
+        case "large":
+          return "text-lg";
+        case "small":
+          return "text-sm";
         default:
-          return 'text-base'
+          return "text-base";
       }
     },
 
@@ -90,78 +84,77 @@ export default {
         this.textClasses,
         this.cursorClasses,
         this.textSize
-      ]
+      ];
     },
 
     bgClasses() {
-      if (this.hasError) return 'bg-red-100'
-      if (this.focussed) return 'bg-gray-100'
-      return 'bg-gray-300'
+      if (this.hasError) return "bg-red-100";
+      if (this.focussed) return "bg-gray-100";
+      return "bg-gray-300";
     },
 
     borderClasses() {
-      if (this.hasError) return 'border border-red-500'
-      return 'border border-gray-300'
+      if (this.hasError) return "border border-red-500";
+      return "border border-gray-300";
     },
 
     shapeClasses() {
-      return 'rounded'
+      return "rounded";
     },
 
     sizeClasses() {
       switch (this.size) {
-        case 'large':
-          return 'h-16 px-4'
-        case 'small':
-          return 'h-8 px-4'
+        case "large":
+          return "h-16 px-4";
+        case "small":
+          return "h-8 px-4";
         default:
-          return 'h-10 px-4'
+          return "h-10 px-4";
       }
     },
 
     textColorClasses() {
-      if (this.hasError) return 'text-red-500'
-      return ''
+      if (this.hasError) return "text-red-500";
+      return "";
     },
 
     textClasses() {
-      if (this.disabled) return 'text-gray-600'
-      if (this.uppercase) return 'uppercase'
-      return ''
+      if (this.disabled) return "text-gray-600";
+      if (this.uppercase) return "uppercase";
+      return "";
     },
 
     cursorClasses() {
-      if (this.disabled) return 'cursor-not-allowed'
-      return ''
+      if (this.disabled) return "cursor-not-allowed";
+      return "";
     },
 
     labelClasses() {
-      return [
-        this.textSize
-      ]
+      return [this.textSize];
     }
   },
 
   methods: {
     valFor(option) {
-      if (typeof option === 'string' || option instanceof String) return option
-      return option.value
+      if (typeof option === "string" || option instanceof String) return option;
+      return option.value;
     },
 
     textFor(option) {
-      if (typeof option === 'string' || option instanceof String) return option
-      return option.text
+      if (typeof option === "string" || option instanceof String) return option;
+      return option.text;
     },
 
     validate(val) {
-      this.errors = []
-      if (this.rules && this.rules.length > 0) this.rules.forEach(rule => {
-        let result = rule(val)
-        if (typeof result === 'string') this.errors.push(result)
-      })
+      this.errors = [];
+      if (this.rules && this.rules.length > 0)
+        this.rules.forEach(rule => {
+          let result = rule(val);
+          if (typeof result === "string") this.errors.push(result);
+        });
     }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -172,7 +165,8 @@ export default {
   outline: 0;
   text-shadow: 0 0 0 #000;
 }
-.bb-select-input:focus, .bb-select-input:active {
+.bb-select-input:focus,
+.bb-select-input:active {
   -moz-appearance: none;
   -webkit-appearance: none;
   appearance: none;
